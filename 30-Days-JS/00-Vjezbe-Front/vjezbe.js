13f472de0c7a4ce7cbf9af80af989ab251fa802f
Andrej Juric <andrej.juric991@gmail.com>
2023-08-22 13:58:51 +0200

    objects

--- 30-Days-JS/00-Vjezbe-Front/vjezbe.js
+++ 30-Days-JS/00-Vjezbe-Front/vjezbe.js
@@ -159,145 +159,9 @@ function totalCost(obj) {
 const totalPrice = totalCost(shoppingCart);
 console.log(totalPrice);
 */
-// Exercises: Level 1 for OBJECTS
-
-// 1. Create an empty object called dog
-
-//let dog = {};
-
-//2. Print the the dog object on the console
-//console.log(dog);
-
-//3. Add name, legs, color, age and bark properties for the dog object. The bark property is a method which return woof woof
-let dog = {
-  name: "Rex",
-  legs: 4,
-  color: "black",
-  age: 2,
-  bark: "woof woof",
-};
-
-// 4. Get name, legs, color, age and bark value from the dog object
-console.log(dog.name, dog.legs, dog.color, dog.bark);
-
-// 5. Set new properties the dog object: breed, getDogInfo
-console.log(dog);
-dog.breed = "doberman";
-console.log(dog);
-
-// Exercises: Level 2
-// 1. Find the person who has many skills in the users object.
-const users = {
-  Alex: {
-    email: "alex@alex.com",
-    skills: ["HTML", "CSS", "JavaScript"],
-    age: 20,
-    isLoggedIn: false,
-    points: 30,
-  },
-  Asab: {
-    email: "asab@asab.com",
-    skills: [
-      "HTML",
-      "CSS",
-      "JavaScript",
-      "Redux",
-      "MongoDB",
-      "Express",
-      "React",
-      "Node",
-    ],
-    age: 25,
-    isLoggedIn: false,
-    points: 50,
-  },
-  Brook: {
-    email: "daniel@daniel.com",
-    skills: ["HTML", "CSS", "JavaScript", "React", "Redux"],
-    age: 30,
-    isLoggedIn: true,
-    points: 50,
-  },
-  Daniel: {
-    email: "daniel@alex.com",
-    skills: ["HTML", "CSS", "JavaScript", "Python"],
-    age: 20,
-    isLoggedIn: false,
-    points: 40,
-  },
-  John: {
-    email: "john@john.com",
-    skills: ["HTML", "CSS", "JavaScript", "React", "Redux", "Node.js"],
-    age: 20,
-    isLoggedIn: true,
-    points: 50,
-  },
-  Thomas: {
-    email: "thomas@thomas.com",
-    skills: ["HTML", "CSS", "JavaScript", "React"],
-    age: 20,
-    isLoggedIn: false,
-    points: 40,
-  },
-  Paul: {
-    email: "paul@paul.com",
-    skills: [
-      "HTML",
-      "CSS",
-      "JavaScript",
-      "MongoDB",
-      "Express",
-      "React",
-      "Node",
-    ],
-    age: 20,
-    isLoggedIn: false,
-    points: 40,
-  },
-};
-//let gradeAbove50 = students.filter((student) => student.grade > 50);
-//let mostSkilledPerson = users.filter((user) => user.skills > 5);
-//console.log(mostSkilledPerson);
-// console.log(users.Alex.skills);
-// console.log(Object.getOwnPropertyNames(users));
-// console.log(users.property);
-
-// console.log(users);
-
-// for (const username in users) {
-//   const user = users[username];
-//   const skills = user.skills;
-//   console.log(`${username} skills: ${skills.join(", ")}`);
-//}
-
-// const array = [1, 2, 3, 4, 5];
-// for (const element of array) {
-//   console.log(element);
-// }
-
-// const obj = { a: 1, b: 2, c: 3 };
-// for (const key in obj) {
-//   console.log(key, obj[key]);
-// }
-let maxSkillUser = 0;
-let countSkill = 0;
-for (const key in users) {
-  const user = users[key];
-  const skills = user.skills;
-  let numSkills = user.skills.length;
-  // console.log(numSkills);
-  let maxSkillUser = key;
-  let countSkill = numSkills;
-  console.log(maxSkillUser, countSkill);
-}
-if (maxSkillUser > 0) {
-  console.log(`${maxSkillUser} ima najviše skillova, ${countSkill}`);
-} else {
-  console.log("bla");
-}
 
 // Array metode zadaci
-
+/*
 let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 // 1. Sum All Numbers: Use the reduce method to sum all the numbers in an array.
@@ -402,3 +266,380 @@ let functionArray = [1, 2, 3, 4, 5, 6];
 
 function movesElement(arr, fromIndex, toIndex) {}
 console.log(movesElement());
+
+console.log();
+
+let car = {
+  mark: "audi",
+};
+
+let car2 = {
+  mark: "audi",
+};
+*/
+
+// 21.8. zadaci
+
+// Array Chunking
+
+/* 
+1. kreirati array
+2. kreirati chunk size => size
+3. kreirati prazni array (array poslije splita)
+4. kreirati funkciju naziva chunkArray s parametrima arr, size koja dijeli array u manje arraye veličine koji odredimo
+5. for petlja za iteraciju kroz array
+6. u petlji sliceani array pushamo u prazni array
+
+
+let normalArray = [10, 20, 30, 40, 50, 60];
+
+function chunkArray(arr, size) {
+  let chunkedArray = [];
+
+  for (let i = 0; i < arr.length; i += size) {
+    let chunk = arr.slice(i, i + size);
+    chunkedArray.push(chunk);
+  }
+  return chunkedArray;
+}
+
+console.log(chunkArray(normalArray, 2));
+
+// Flatten an Array
+
+let nested = [10, 20, 30, [40, 50, 60, 70, 80, 90]];
+
+function flatten(arr) {
+  let flattened = arr.reduce(
+    (accumulator, currentValue) => accumulator.concat(currentValue),
+    []
+  );
+  // let flattened = arr.flat(2);
+  return flattened;
+}
+
+console.log(flatten(nested));
+// Flattening an array of arrays. Use flat() instead. :D
+
+// Find the Most Frequent Element
+
+
+//1. funkcija koja vraća element koji se najčešće pojavljuje
+// 2. kreirati array i funkciju naziva mostFrequent(arr)
+
+let frequentArray = [10, 20, 30, 20, 40, 50, 20, 60];
+console.log(frequentArray.map((element) => frequentArray.indexOf(element)));
+let maxCount = [];
+
+// funkcija
+function mostFrequent(arr) {
+  // let num = arr.map((element) => element);
+  // num.sort();
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j == i; i++) {
+      if (i == arr.indexOf() && j == arr.indexOf()) {
+        arr.push(maxCount);
+      }
+      console.log(maxCount);
+    }
+  }
+  return frequentArray;
+}
+
+console.log(mostFrequent(frequentArray));
+*/
+//
+/*
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const eurToUsd = 1.1;
+
+const movementsUSD = movements.map(function (mov) {
+  return mov * eurToUsd;
+});
+console.log(movements);
+console.log(movementsUSD);
+
+const movementsUSDfor = [];
+for (const mov of movements) movementsUSDfor.push(mov * eurToUsd);
+console.log(movementsUSDfor);
+
+
+const andrej = {
+  firstName: "Andrej",
+  lastName: "Jurić",
+  age: 2023 - 1991,
+  job: "frontend developer",
+  friends: ["Mihaela", "Stipo", "Kajo"],
+};
+console.log(andrej);
+
+console.log(andrej.lastName);
+console.log(andrej["lastName"]);
+
+const nameKey = "Name";
+console.log(andrej["first" + nameKey]);
+console.log(andrej["last" + nameKey]);
+
+// const interestedIn = prompt(
+//   "What do you want to know about Andrej? Choose between firstName, lastName, age, job and friends"
+// );
+
+// if (andrej[interestedIn]) {
+//   console.log(andrej[interestedIn]);
+// } else {
+//   console.log(
+//     "Wrong request ! Choose between firstName, lastName, age, job and friends"
+//   );
+// }
+
+andrej.location = "Osijek";
+console.log(andrej);
+
+console.log(
+  `${andrej.firstName} has ${andrej.friends.length} friends and his best friend is called ${andrej.friends[0]}.`
+);
+
+
+const andrej = {
+  firstName: "Andrej",
+  lastName: "Jurić",
+  birthYear: 1991,
+  job: "frontend developer",
+  friends: ["Mihaela", "Stipo", "Kajo"],
+  hasDriverLicence: true,
+
+  // calcAge: function (birthYear) {
+  //   return 2023 - birthYear;
+  // },
+  // calcAge: function () {
+  //   console.log(this);
+  //   return 2023 - this.birthYear;
+  // },
+  calcAge: function () {
+    this.age = 2023 - this.birthYear;
+    return this.age;
+  },
+  getSummary: function () {
+    return `${this.firstName} is a ${this.calcAge()} year old and he has ${
+      this.hasDriverLicence ? "a" : "no"
+    } driver license.`;
+  },
+};
+
+console.log(andrej.calcAge(1991));
+console.log(andrej);
+
+console.log(andrej.getSummary());
+
+
+// Exercises: Level 1 for OBJECTS
+
+// 1. Create an empty object called dog
+
+//let dog = {};
+
+//2. Print the the dog object on the console
+//console.log(dog);
+
+//3. Add name, legs, color, age and bark properties for the dog object. The bark property is a method which return woof woof
+let dog = {
+  name: "Rex",
+  legs: 4,
+  color: "black",
+  age: 2,
+  bark: "woof woof",
+};
+
+// 4. Get name, legs, color, age and bark value from the dog object
+console.log(dog.name, dog.legs, dog.color, dog.bark);
+
+// 5. Set new properties the dog object: breed, getDogInfo
+console.log(dog);
+dog.breed = "doberman";
+console.log(dog);
+
+// Exercises: Level 2
+// 1. Find the person who has many skills in the users object.
+const users = {
+  Alex: {
+    email: "alex@alex.com",
+    skills: ["HTML", "CSS", "JavaScript"],
+    age: 20,
+    isLoggedIn: false,
+    points: 30,
+  },
+  Asab: {
+    email: "asab@asab.com",
+    skills: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "Redux",
+      "MongoDB",
+      "Express",
+      "React",
+      "Node",
+    ],
+    age: 25,
+    isLoggedIn: false,
+    points: 50,
+  },
+  Brook: {
+    email: "daniel@daniel.com",
+    skills: ["HTML", "CSS", "JavaScript", "React", "Redux"],
+    age: 30,
+    isLoggedIn: true,
+    points: 50,
+  },
+  Daniel: {
+    email: "daniel@alex.com",
+    skills: ["HTML", "CSS", "JavaScript", "Python"],
+    age: 20,
+    isLoggedIn: false,
+    points: 40,
+  },
+  John: {
+    email: "john@john.com",
+    skills: ["HTML", "CSS", "JavaScript", "React", "Redux", "Node.js"],
+    age: 20,
+    isLoggedIn: true,
+    points: 50,
+  },
+  Thomas: {
+    email: "thomas@thomas.com",
+    skills: ["HTML", "CSS", "JavaScript", "React"],
+    age: 20,
+    isLoggedIn: false,
+    points: 40,
+  },
+  Paul: {
+    email: "paul@paul.com",
+    skills: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "MongoDB",
+      "Express",
+      "React",
+      "Node",
+    ],
+    age: 20,
+    isLoggedIn: false,
+    points: 40,
+  },
+};
+let maxSkills = 0;
+let mostSkilledUser = null;
+
+// 1. Find the person who has many skills in the users object.
+const result = Object.fromEntries([
+  Object.entries(users).sort(
+    (a, b) => b[1].skills.length - a[1].skills.length
+  )[0],
+]);
+
+console.log(result);
+
+// 2. Count logged in users, count users having greater than equal to 50 points from the following object.
+let countLoggedUsers = Object.values(users).filter(
+  (user) => user.isLoggedIn
+).length;
+console.log(countLoggedUsers);
+
+let usersGreaterThan50 = Object.values(users).filter(
+  (user) => user.points >= 50
+);
+console.log(usersGreaterThan50.length);
+
+// 3. Find people who are MERN stack developer from the users object
+let findMERN = Object.values(users).find((user) =>
+  user.skills.includes("MERN")
+);
+if (findMERN) {
+  console.log(findMERN);
+} else {
+  console.log("There is no users who are MERN stack developer");
+}
+
+// 4. Set your name in the users object without modifying the original users object
+
+users.Andrej = "Andrej";
+console.log(users);
+
+const updatedUsers = {
+  ...users,
+  Andrej: {
+    skills: ["JavaScript"],
+    age: 31,
+    isLoggedIn: true,
+    points: 100,
+  },
+};
+
+console.log(updatedUsers);
+
+// 5. Get all keys or properties of users object
+
+let allUserKeys = Object.keys(updatedUsers);
+console.log(allUserKeys);
+
+// 6. Get all the values of users object
+
+let allUserValues = Object.values(updatedUsers);
+console.log(allUserValues);
+
+// 7. Use the countries object to print a country name, capital, populations and languages.
+*/
+// Exercises: Level 3
+
+// 1. Create an object literal called personAccount. It has firstName, lastName, incomes, expenses properties and it has totalIncome, totalExpense, accountInfo,addIncome, addExpense and accountBalance methods. Incomes is a set of incomes and its description and expenses is a set of incomes and its description.
+
+const personAccount = {
+  firstName: "Andrej",
+  lastName: "Jurić",
+  incomes: [200, 100, 300, 500],
+  expenses: [50, 20, 10, 30],
+  totalIncome: function () {
+    let totalIncome = 0;
+    for (let i = 0; i < this.incomes.length; i++) {
+      totalIncome += this.incomes[i];
+    }
+    return totalIncome;
+  },
+  totalExpense: function () {
+    let totalExpense = 0;
+    for (let i = 0; i < this.expenses.length; i++) {
+      totalExpense += this.expenses[i];
+    }
+    return totalExpense;
+  },
+  accountInfo: function () {
+    return console.log(
+      `This is account information of or client ${
+        this.firstName
+      }. His last name is ${this.lastName}. Incomes: ${this.totalIncome()}.`
+    );
+  },
+  addIncome: function (income) {
+    return this.incomes.push(income);
+  },
+  addExpense: function (expense) {
+    return this.expenses.push(expense);
+  },
+  accountBalance: function () {
+    personAccount.balance = this.incomes - this.expenses;
+    console.log(this);
+    return this.balance;
+  },
+};
+console.log(personAccount.totalIncome());
+console.log(personAccount.totalExpense());
+console.log(personAccount.accountInfo());
+
+console.log(personAccount.addIncome(200));
+console.log(personAccount.incomes);
+
+console.log(personAccount.addExpense(25));
+console.log(personAccount.expenses);
+console.log(personAccount.accountBalance());
