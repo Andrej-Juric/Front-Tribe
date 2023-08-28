143cef60ad818d9ab76f0d998b8c8d51d4f78931
Andrej Juric <andrej.juric991@gmail.com>
2023-08-28 10:52:46 +0200

    json

--- 30-Days-JS/day15-classes.js
+++ 30-Days-JS/day15-classes.js
@@ -75,7 +75,7 @@ console.log(pet1.saySomething());
 pet1.setSkill = "Lajanje";
 pet1.setSkill = "Trčanje";
 
-*/
+
 
 class Animal {
   constructor(name, age, color, legs, food) {
@@ -113,3 +113,76 @@ const cat = new Cat("Mini", 2, "grey", 4);
 console.table(cat);
 
 //
+
+class Person {
+  constructor(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  }
+}
+
+const andrej = new Person("Andrej", 1991);
+console.log(andrej);
+
+const matilda = new Person("Matilda", 1995);
+const jack = new Person("Jack", 2000);
+console.log(matilda, jack);
+
+// Prototypes
+console.log(Person.prototype);
+
+Person.prototype.calcAge = function () {
+  console.log(2023 - this.birthYear);
+};
+
+andrej.calcAge();
+matilda.calcAge();
+jack.calcAge();
+*/
+
+// class expression
+//const PersonCl = class {}
+
+// class declaration
+class PersonCl {
+  constructor(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  }
+
+  calcAge() {
+    console.log(2023 - this.birthYear);
+  }
+
+  greet() {
+    console.log(`Hello ${this.firstName}`);
+  }
+
+  get age() {
+    return 2023 - this.birthYear;
+  }
+}
+
+let mihaela = new PersonCl("Mihaela", 1997);
+console.log(mihaela);
+mihaela.calcAge();
+mihaela.greet();
+console.log(mihaela.age);
+console.log(mihaela.__proto__ === PersonCl.prototype);
+
+const account = {
+  owner: "andrej",
+  movements: [200, 530, 120, 300],
+
+  get latest() {
+    return this.movements.slice(-1).pop();
+  },
+
+  set latest(mov) {
+    this.movements.push(mov);
+  },
+};
+console.log(account.latest);
+
+account.latest = 50;
+console.log(account.movements);
---
+++ 30-Days-JS/day16-json.js
@@ -0,0 +1,36 @@
+// javascript object notation
+
+// JSON to object -> JSON.parse()
+
+// Object to JSON -> JSON.stringify()
+
+const usersText = `{
+    "users":[
+      {
+        "firstName":"Asabeneh",
+        "lastName":"Yetayeh",
+        "age":250,
+        "email":"asab@asb.com"
+      },
+      {
+        "firstName":"Alex",
+        "lastName":"James",
+        "age":25,
+        "email":"alex@alex.com"
+      },
+      {
+      "firstName":"Lidiya",
+      "lastName":"Tekle",
+      "age":28,
+      "email":"lidiya@lidiya.com"
+      }
+    ]
+    }`;
+
+const usersObj = JSON.parse(usersText, undefined, 4);
+console.log(usersObj);
+
+// to string
+
+const txt = JSON.stringify(usersText, undefined, 4);
+console.log(txt);
